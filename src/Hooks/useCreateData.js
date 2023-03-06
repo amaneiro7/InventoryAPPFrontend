6b170ccea225d0a10db1dad605d2b9d8ff7d27a5
Andres Maneiro <amaneiro7@gmail.com>
2023-03-05 21:06:38 -0400

    creacion de custom hooks para actualizar, crear y eliminar data

---
+++ src/Hooks/useCreateData.js
@@ -0,0 +1,50 @@
+import { useEffect, useReducer } from "react";
+import { createItems } from "services/api";
+import { getApiUrl } from "services/config";
+
+//Se Deffine el estado inicial
+const initialState = {
+    status: "",
+    statusInfo: "",
+    loading: false,
+    error: null,
+}
+
+const reducer = (state, action) => {    
+    return reducerOBJECT(state, action.payload)[action.type] || state
+}
+const reducerOBJECT = (state, payload) => ({
+    'START': {
+        ...state,
+        loading: true,
+    },
+    'SUCCESS': {
+        ...state,
+        loading: false,
+        status: "Elemento creado exitosamente",
+        statusInfo: payload
+    },
+    'ERROR': {
+        ...state,
+        loading: false,
+        error: payload,
+    }
+})
+
+export default function useCreateData({ endPoint, data }) {
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+    const onStart = () => dispatch({ type: 'START' })
+    const onSuccess = (res) => dispatch({ type: 'SUCCESS', payload: res})
+    const onError = (error) => dispatch({ type: 'ERROR', payload: error })
+
+
+    useEffect(() => {
+        onStart()        
+        createItems({ path: `${getApiUrl}${endPoint}`, data })            
+            .then(res => onSuccess(res))
+            .catch(error => onError(error))
+    }, [endPoint]);
+    
+    return state
+}
